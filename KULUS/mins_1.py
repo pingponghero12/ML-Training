46cb6a82c56bb00ae71107d80e7441d4d83bfd02
Kacper Kulawiak <sekkipl@gmail.com>
2024-07-23 14:29:30 +0200

    Workink mnist tensorflow

--- KULUS/mins_1.py
+++ KULUS/mins_1.py
@@ -1,6 +1,23 @@
+import os 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
+
 import tensorflow as tf
+from tensorflow.keras import layers, models
+tf.experimental.numpy.experimental_enable_numpy_behavior()
 
-x= tf.constant(7,dtype=tf.float32)
-x = tf.constant([[1,2,3],[7,8,9]])
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.mnist.load_data()
 
-print(x)
+train_images =train_images.reshape((60000, 28, 28, 1)).astype('float32') / 255
+test_images = test_images.reshape((10000, 28, 28, 1)).astype('float32') / 255
+
+model = models.Sequential()
+
+
+model.add(layers.Flatten(input_shape=(28,28)))
+model.add(layers.Dense(256, activation="relu"))
+model.add(layers.Dense(10,activation = "softmax"))
+
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=5, batch_size=64, validation_split=0.2)
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+print(f'Test accuracy: {test_acc}')
